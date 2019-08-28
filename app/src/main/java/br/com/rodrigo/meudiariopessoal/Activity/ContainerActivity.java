f6a83be43bc78a0fd0ccdc1fe755943c791c407a
RodrigoBP99 <rodrigobozapontes@gmail.com>
2019-08-28 17:18:11 -0300

    Cria activity pra cadastro e activity principal

---
+++ app/src/main/java/br/com/rodrigo/meudiariopessoal/Activity/ContainerActivity.java
@@ -0,0 +1,15 @@
+package br.com.rodrigo.meudiariopessoal.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.com.rodrigo.meudiariopessoal.R;
+
+public class ContainerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_container);
+    }
+}
--- app/src/main/java/br/com/rodrigo/meudiariopessoal/Activity/MainActivity.java
+++ app/src/main/java/br/com/rodrigo/meudiariopessoal/Activity/MainActivity.java
@@ -1,7 +1,9 @@
-package br.com.rodrigo.meudiariopessoal;
+package br.com.rodrigo.meudiariopessoal.Activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import br.com.rodrigo.meudiariopessoal.R;
 
 public class MainActivity extends AppCompatActivity {
 
