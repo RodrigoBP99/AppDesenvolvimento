ac000fee055d35317d8cccc676653ee57d3a51a7
RodrigoBP99 <rodrigobozapontes@gmail.com>
2019-08-28 17:07:09 -0300

    Cria model para Usuario

---
+++ app/src/main/java/br/com/rodrigo/meudiariopessoal/Model/User.java
@@ -0,0 +1,46 @@
+package br.com.rodrigo.meudiariopessoal.Model;
+
+public class User {
+    private int id, telefone;
+    private String nome, email, senha;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(int telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
