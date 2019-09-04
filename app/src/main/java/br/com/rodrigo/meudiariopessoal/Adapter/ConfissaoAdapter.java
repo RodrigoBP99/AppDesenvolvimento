cc11f126880bfbcb0fd8990f3e51d3b8685626b0
RodrigoBP99 <rodrigobozapontes@gmail.com>
2019-09-04 14:00:14 -0300

    Mudança layout adapter e remoção do onLongClick no adapter

--- app/src/main/java/br/com/rodrigo/meudiariopessoal/Adapter/ConfissaoAdapter.java
+++ app/src/main/java/br/com/rodrigo/meudiariopessoal/Adapter/ConfissaoAdapter.java
@@ -1,10 +1,13 @@
 package br.com.rodrigo.meudiariopessoal.Adapter;
 
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.arch.persistence.room.Room;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,8 +15,11 @@ import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 import java.util.ArrayList;
 
+import br.com.rodrigo.meudiariopessoal.Activity.ContainerActivity;
 import br.com.rodrigo.meudiariopessoal.DAO.AppDatabase;
 import br.com.rodrigo.meudiariopessoal.Model.Confissao;
 import br.com.rodrigo.meudiariopessoal.R;
@@ -38,7 +44,9 @@ public class ConfissaoAdapter extends RecyclerView.Adapter<ConfissaoAdapter.Conf
     @Override
     public void onBindViewHolder(@NonNull ConfissaoViewHolder confissaoViewHolder, int posicao) {
         final Confissao confissao = confissaoArrayList.get(posicao);
+        final AppDatabase appDatabase = AppDatabase.getInstance(context);
 
+        confissaoViewHolder.textViewUserName.setText(confissao.getUserID());
         confissaoViewHolder.textViewConfissao.setText(confissao.getTexto());
         confissaoViewHolder.textViewData.setText(confissao.getData());
         confissaoViewHolder.textViewHora.setText(confissao.getHora());
@@ -50,29 +58,11 @@ public class ConfissaoAdapter extends RecyclerView.Adapter<ConfissaoAdapter.Conf
                 alerta.setTitle("Apagar").setPositiveButton("Sim", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        AppDatabase appDatabase = Room.databaseBuilder(context, AppDatabase.class, "confissaoDataBase")
-                                .allowMainThreadQueries()
-                                .build();
                         appDatabase.confissaoDao().delete(confissao);
+                        context.startActivity(new Intent(context, ContainerActivity.class));
+                        ((Activity)context).finish();
                     }
                 }).setNegativeButton("Não", null).setMessage("Deseja excluir?").show();
-            }
-        });
-
-        confissaoViewHolder.itemView.setOnLongClickListener(new View.OnLongClickListener() {
-            @Override
-            public boolean onLongClick(View v) {
-                AlertDialog.Builder alerta = new AlertDialog.Builder(context);
-                alerta.setTitle("Apagar").setPositiveButton("Sim", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        AppDatabase appDatabase = Room.databaseBuilder(context, AppDatabase.class, "confissaoDataBase")
-                                .allowMainThreadQueries()
-                                .build();
-                        appDatabase.confissaoDao().delete(confissao);
-                    }
-                }).setNegativeButton("Não", null).setMessage("Deseja excluir?").show();
-                return false;
             }
         });
     }
@@ -83,15 +73,16 @@ public class ConfissaoAdapter extends RecyclerView.Adapter<ConfissaoAdapter.Conf
     }
 
     public class ConfissaoViewHolder extends RecyclerView.ViewHolder {
+        private TextView textViewUserName;
         private TextView textViewConfissao;
         private TextView textViewData;
         private TextView textViewHora;
         private ImageButton imageButtonDelete;
 
-
         public ConfissaoViewHolder(@NonNull View itemView) {
             super(itemView);
 
+            textViewUserName = itemView.findViewById(R.id.textViewUserName);
             textViewConfissao = itemView.findViewById(R.id.textViewConfissao);
             textViewData = itemView.findViewById(R.id.textViewData);
             textViewHora = itemView.findViewById(R.id.textViewHora);
